9fae63a33e6c6e6e0a2175051a1bcc55a8aeb311
harmakit <harmakit@gmail.com>
2022-11-17 20:10:12 +0800

    upd menu and requirements.txt

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from command.update import update
 from service.data_provider import DataProvider, DataProviderSource, DataProviderAlgorithm
 from service.diff_guesser import DiffGuesser
 
-actions = ["[r] run recognizer", "[t] test recognizer", "[u] update data from images"]
+actions = ["[r] run recognizer", "[t] test recognizer", "[u] update data from images", "[e] exit"]
 actions_menu = TerminalMenu(actions, title="Choose action")
 
 data_sources = ["[d] data dir", "[m] mnist dataset"]
@@ -15,30 +15,34 @@ data_sources_menu = TerminalMenu(data_sources, title="Choose data source")
 data_algorithms = ["[c] cumulative", "[a] average"]
 data_algorithms_menu = TerminalMenu(data_algorithms, title="Choose data provider algorithm")
 
-menu_entry_index = actions_menu.show()
+while True:
+    menu_entry_index = actions_menu.show()
 
-data_provider_source = DataProviderSource.MNIST
-data_provider_algorithm = DataProviderAlgorithm.CUMULATIVE
+    if menu_entry_index == 3:
+        exit()
 
-if menu_entry_index == 0 or menu_entry_index == 1:
-    data_source_index = data_sources_menu.show()
-    if data_source_index == 0:
-        data_provider_source = DataProviderSource.DATADIR
-    elif data_source_index == 1:
-        data_provider_source = DataProviderSource.MNIST
+    data_provider_source = DataProviderSource.MNIST
+    data_provider_algorithm = DataProviderAlgorithm.CUMULATIVE
 
-    data_algorithm_index = data_algorithms_menu.show()
-    if data_algorithm_index == 0:
-        data_provider_algorithm = DataProviderAlgorithm.CUMULATIVE
-    elif data_algorithm_index == 1:
-        data_provider_algorithm = DataProviderAlgorithm.AVG
+    if menu_entry_index == 0 or menu_entry_index == 1:
+        data_source_index = data_sources_menu.show()
+        if data_source_index == 0:
+            data_provider_source = DataProviderSource.DATADIR
+        elif data_source_index == 1:
+            data_provider_source = DataProviderSource.MNIST
 
-dp = DataProvider(data_provider_source, data_provider_algorithm)
-guesser = DiffGuesser()
+        data_algorithm_index = data_algorithms_menu.show()
+        if data_algorithm_index == 0:
+            data_provider_algorithm = DataProviderAlgorithm.CUMULATIVE
+        elif data_algorithm_index == 1:
+            data_provider_algorithm = DataProviderAlgorithm.AVG
 
-if menu_entry_index == 0:  # run
-    run(dp, guesser)
-elif menu_entry_index == 1:  # test
-    test(dp, guesser)
-elif menu_entry_index == 2:  # update
-    update()
+    dp = DataProvider(data_provider_source, data_provider_algorithm)
+    guesser = DiffGuesser()
+
+    if menu_entry_index == 0:  # run
+        run(dp, guesser)
+    elif menu_entry_index == 1:  # test
+        test(dp, guesser)
+    elif menu_entry_index == 2:  # update
+        update()
